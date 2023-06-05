9b0611886cca34c20ae0ed15839b0c5a03e5ce17
rana271 <ranacomp@gmail.com>
2023-06-05 09:01:57 +0530

    Add project files.

---
+++ src/app/Employee.ts
@@ -0,0 +1,9 @@
+export interface IEmployee {
+    code: string;
+    name: string;
+    gender: string;
+    anualSalary: number;
+    dateofBirth: string;
+
+
+}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent {
+name:string="Rana";
+helloworld
+()
+:void{
+  alert(this.name);
+};
+}
